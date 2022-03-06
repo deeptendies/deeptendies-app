a7689c313819bb7a004c6cf7114b2f6c53924541
Erika Wang <801erikawang@gmail.com>
2022-03-06 15:20:50 -0700

    renamed project

--- tendieproject/asgi.py
+++ tendieproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for deeptendiessite project.
+ASGI config for tendieproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deeptendiessite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tendieproject.settings')
 
 application = get_asgi_application()
